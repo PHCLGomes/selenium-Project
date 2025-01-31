2cb7a05bcdffad18f73d712e405893ddc488a476
Pedro Henrique <122324968+PHCLGomes@users.noreply.github.com>
2025-02-01 09:12:13 +1100

    upgrade project

--- selenium-project/src/test/java/runner/runner.java
+++ selenium-project/src/test/java/runner/runner.java
@@ -1,14 +1,9 @@
 package runner;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeAll;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
